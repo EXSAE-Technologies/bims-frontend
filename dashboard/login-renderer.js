7aab3c219b237442502284639f53992d9df9ab09
fshangala <fshangala@gmail.com>
2022-11-19 09:05:23 +0530

    login url updated

---
+++ dashboard/login-renderer.js
@@ -0,0 +1,31 @@
+var wrapper = betapi.apiWrapper()
+
+        document.querySelector("#loginButton").addEventListener("click",()=>{
+            document.querySelector("#loginButton").innerHTML="Logging in..."
+            document.querySelector("#loginButton").setAttribute("disabled","true")
+            fetch("http://server.iitsar.com:8000/api-token-auth/",{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username:document.querySelector("#inputUsername").value,
+                    password:document.querySelector("#inpuptPassword").value
+                })
+            }).then((response)=>response.json()).then((data)=>{
+                if(data.token){
+                    localStorage.setItem("token",data.token)
+                    location.assign("index.html")
+                } else {
+                    alert(data.detail)
+                }
+                
+                document.querySelector("#loginButton").innerHTML="Login"
+                document.querySelector("#loginButton").removeAttribute("disabled")
+            }).catch((error)=>{
+                alert(error)
+                
+                document.querySelector("#loginButton").innerHTML="Login"
+                document.querySelector("#loginButton").removeAttribute("disabled")
+            })
+        })
